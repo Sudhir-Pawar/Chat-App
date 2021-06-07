bb2d1e81e920b7686d0ea3f2a4d72e835d464148
Sudhir-Pawar <pawarsudhir8498@gmail.com>
2021-06-07 23:27:26 +0530

    Update script to start & stop all running node application.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ssh -i ~/chat-app-demo.pem ubuntu@ec2-54-85-103-151.compute-1.amazonaws.com << 'ENDSSH'
-pm2 stop chat-app
+ssh -i ~/chat-app-demo.pem ubuntu@ec2-54-210-16-175.compute-1.amazonaws.com << 'ENDSSH'
+pm2 stop all
 rm -rf ~/workspace/Chat-App/
 cd ~/workspace/
 git clone https://github.com/Sudhir-Pawar/Chat-App.git
 cd Chat-App
 npm i
-pm2 start chat-app
+pm2 start all
 ENDSSH
